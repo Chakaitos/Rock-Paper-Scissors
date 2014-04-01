d0dfe3d1d709683c0f9eecdc15658b48c56a38c5
Christopher Palmer <snly2386@gmail.com>
2014-04-01 15:47:19 -0500

    refactored again

--- lib/rps/game.rb
+++ lib/rps/game.rb
@@ -1,4 +1,4 @@
-class RPS
+class Game
   attr_reader :player1, :player2
 
   def initialize(player1, player2)
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,12 +1,12 @@
 require './spec/spec_helper.rb'
 
-describe RPS do
+describe Game do
 
   describe "initialize" do
     it "adds two players to the game" do
       john = User.new("John")
       drew = User.new("Drew")
-      game = RPS.new(john, drew)
+      game = Game.new(john, drew)
       expect(game.player1.name).to eq (john.name)
       expect(game.player2.name).to eq (drew.name)
     end
