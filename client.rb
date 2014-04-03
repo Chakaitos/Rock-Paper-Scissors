809e2789b111b255755f85009972e76c0c920ec6
Drew Garcia <drewgarcia23@gmail.com>
2014-04-03 15:09:05 -0500

    Ability to sign in and sign up on terminal

--- client.rb
+++ client.rb
@@ -8,49 +8,78 @@ module RPS
 		def self.start
 			@db = RPS.db
 
-			puts " __      __       .__                                     __              ________                                ____   ___/\          ____________________  _________"
-			puts "/  \    /  \ ____ |  |   ____  ____   _____   ____      _/  |_  ____      \______ \_______   ______  _  __        \   \ /   )/_____     \______   \______   \/   _____/"
-			puts "\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \     \   __\/  _ \      |    |  \_  __ \_/ __ \ \/ \/ /  ______ \   Y   //  ___/      |       _/|     ___/\_____  \	"
-			puts " \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/      |  | (  <_> )     |    `   \  | \/\  ___/\     /  /_____/  \     / \___ \       |    |   \|    |    /        \ "
-			puts "	\__/\  /  \___  >____/\___  >____/|__|_|  /\___  >     |__|  \____/     /_______  /__|    \___  >\/\_/             \___/ /____  >      |____|_  /|____|   /_______  /"
-			puts "       \/       \/          \/            \/     \/                               \/            \/                              \/              \/                  \/ "
-			puts
-			puts "[1] Sign in\n[2] Sign up"
+			puts "   ▄████████    ▄███████▄    ▄████████ "
+			puts "  ███    ███   ███    ███   ███    ███ "
+			puts "  ███    ███   ███    ███   ███    █▀  "
+			puts " ▄███▄▄▄▄██▀   ███    ███   ███        "
+			puts "▀▀███▀▀▀▀▀   ▀█████████▀  ▀███████████ "
+			puts "▀███████████   ███                 ███ "
+			puts "  ███    ███   ███           ▄█    ███ "
+			puts "  ███    ███  ▄████▀       ▄████████▀  "
 
-			sign_command = gets.chomp.to_i
+			puts ""
 
-			if sign_command == 1
-				puts "Enter your username"
-				name = gets.chomp
-				puts "Enter your password"
-				password = gets.chomp
-				result = RPS::SignIn.run({:name => name, :password => password})
-			  if result.success?
-			  	puts "#{result.user.name} has signed in successfully."
-			  else
-			  	if result.error == :invalid_name
-			  		puts "That username does not exist."
-			  	elsif result.error == :invalid_password
-			  		puts "The password is incorrect"
-			  	end
-			  end
-			elsif sign_command == 2
-				puts "Enter your username"
-				name = gets.chomp
-				puts "Enter your password"
-				password = gets.chomp
-				result = RPS::SignIn.run({:name => name, :password => password})
-			  if result.success?
-			  	puts "#{result.user.name} successfully signed up, please log in to play."
-			  else
-			  	if result.error == :name_taken
-			  		puts "That username already exists."
-			  	elsif result.error == :weak_password
-			  		puts "The password has to be at least 3 characters"
-			  	end
-			  end
+			puts "[1] Sign in\n[2] Sign up \n[3] To Exit"
+
+			sign_command = gets.chomp
+
+			if sign_command.to_i == 1
+				RPS::TerminalClient.sign_in
+			elsif sign_command.to_i == 2
+				RPS::TerminalClient.sign_up
+			elsif sign_command.to_i == 3
+				puts "Have a nice day!"
+			else
+				puts "Wrong Input!"
+				RPS::TerminalClient.start
 			end
 
+		end #Ends the start method
+
+		def self.sign_in
+			puts "Username:"
+			name = gets.chomp
+			if name == "q"
+				RPS::TerminalClient.start
+			else
+			puts "Password:"
+			password = gets.chomp
+			result = RPS::SignIn.run({:name => name, :password => password})
+		  if result.success?
+		  	@session_id = result.session_id
+		  	puts "#{name} has signed in successfully."
+		  	RPS::TerminalClient.help
+		  else
+		  	if result.error == :invalid_name
+		  		puts "That username does not exist. Please register..."
+		  		RPS::TerminalClient.sign_up
+		  	elsif result.error == :invalid_password
+		  		puts "The password is incorrect. Please retype your credentials or press q to quit"
+		  		RPS::TerminalClient.sign_in
+		  	end
+		  end
+		end
+	end
+
+		def self.sign_up
+			puts "Username:"
+			name = gets.chomp
+			puts "Password:"
+			password = gets.chomp
+			result = RPS::SignUp.run({:name => name, :password => password})
+		  if result.success?
+		  	puts "#{result.user.name} successfully signed up, please log in to play."
+		    RPS::TerminalClient.sign_in
+		  else
+		  	if result.error == :name_taken
+		  		puts "That username already exists."
+		  	elsif result.error == :weak_password
+		  		puts "The password has to be at least 3 characters"
+		  	end
+		  end
+		end
+
+		def self.run_commands
 			command = gets.chomp
 
 		  until command == 'quit' || command == 'q'
@@ -59,19 +88,19 @@ module RPS
 				action = command.split(' ')[1].downcase
 				params = command.split(' ')[2..-1].join(' ')
 
-				if command == 'help'
-				  TM::TerminalClient.help
-				  command = gets.chomp
-				end
+				# if command == 'help'
+				#   RPS::TerminalClient.help
+				#   command = gets.chomp
+				# end
 
 		  	case category
 		  	when 'invite'
 		  		case action
 				  when "create"
 				  	user_to_invite = params[0]
-					  result = RPS::CreateInvite.run({ :session_id => @session.id, :invitee_id => user_to_invite})
+					  result = RPS::CreateInvite.run({ :session_id => @session_id, :invitee_id => user_to_invite})
 					  if result.success?
-					  	puts "Invite sent from #{result.inviter.name} to #{result.inviter.name}."
+					  	puts "Invite sent from #{result.inviter.name} to #{result.invitee.name}."
 					  else
 					  	if result.error == :invitee_not_found
 					  		puts "Invitee not found!"
@@ -82,10 +111,10 @@ module RPS
 				end
 
 
-			end
-		end
+			end #Ends the until loop
+		end #Ends the run commands method
 
-		def help
+		def self.help
 			puts "Available Commands:
   			\tusers list - List all users
   			\tmatch list - List active matches
@@ -94,6 +123,7 @@ module RPS
   			\tinvite accept IID - Accept invite with id=IID
   			\tinvite create USERNAME - Invite user with username=USERNAME to play a game
 				\tmatch list"
+				RPS::TerminalClient.run_commands
 		end
 
 
@@ -103,5 +133,4 @@ module RPS
 
 	end
 end
-
 RPS::TerminalClient.start
