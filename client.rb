463c16c9e723f1316452a6892d63a28841371eab
Drew Garcia <drewgarcia23@gmail.com>
2014-04-03 11:17:38 -0500

    Terminal words

--- client.rb
+++ client.rb
@@ -1,4 +1,7 @@
-require_relative 'lib/rps.rb'
+require_relative './lib/rps.rb'
+require 'io/console'
+
+
 module RPS
 
 	class TerminalClient
@@ -7,7 +10,7 @@ module RPS
 
 			puts " __      __       .__                                     __              ________                                ____   ___/\          ____________________  _________
 						/  \    /  \ ____ |  |   ____  ____   _____   ____      _/  |_  ____      \______ \_______   ______  _  __        \   \ /   )/_____     \______   \______   \/   _____/
-						\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \     \   __\/  _ \      |    |  \_  __ \_/ __ \ \/ \/ /  ______ \   Y   //  ___/      |       _/|     ___/\_____  \ 
+						\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \     \   __\/  _ \      |    |  \_  __ \_/ __ \ \/ \/ /  ______ \   Y   //  ___/      |       _/|     ___/\_____  \
 						 \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/      |  | (  <_> )     |    `   \  | \/\  ___/\     /  /_____/  \     / \___ \       |    |   \|    |    /        \
 						  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >     |__|  \____/     /_______  /__|    \___  >\/\_/             \___/ /____  >      |____|_  /|____|   /_______  /
 						       \/       \/          \/            \/     \/                               \/            \/                              \/              \/                  \/ "
@@ -30,22 +33,22 @@ module RPS
 		  	when 'project'
 		  		case action
 					when "list"
-					  TM::TerminalClient.project_list
+					  RPS::TerminalClient.project_list
 					  command = gets.chomp
 				  when "create"
-					  TM::TerminalClient.project_create(params)
+					  RPS::TerminalClient.project_create(params)
 					  command = gets.chomp
 					when "show"
-					  TM::TerminalClient.project_show(params)
+					  RPS::TerminalClient.project_show(params)
 					  command = gets.chomp
 					when "history"
-					  TM::TerminalClient.project_history(params)
+					  RPS::TerminalClient.project_history(params)
 					  command = gets.chomp
 					when "recruit"
-						TM::TerminalClient.project_recruit(params)
+						RPS::TerminalClient.project_recruit(params)
 					  command = gets.chomp
 					when "employees"
-						TM::TerminalClient.project_employees(params)
+						RPS::TerminalClient.project_employees(params)
 					  command = gets.chomp
 					end
 
@@ -53,24 +56,17 @@ module RPS
 			end
 		end
 
-		def self.help 
+		def help
 			puts "Available Commands:
-		  \thelp - Show these commands again
-		  \tproject list - List all projects
-		  \tproject create <NAME> - Create a new project with name=NAME
-		  \tproject show <PID> - Show remaining tasks for project with id=PID
-		  \tproject history <PID> - Show completed tasks for project with id=PID
-		  \tproject employees <PID> - Show employees participating in this project
-		  \tproject recruit <PID> <EID> - Adds employee EID to participate in project PID
-		  \ttask create <PID> <PRIORITY> <DESC> - Add a new task to project with id=PID
-		  \ttask assign <TID> <EID> - Assign task TID to employee EID
-		  \ttask mark <TID> - Mark task with id=TID as complete
-		  \temp list - List all employees
-		  \temp create <NAME> - Create a new employee
-		  \temp show <EID> - Show employee EID and all participating projects
-		  \temp details <EID> - Show all remaining tasks assigned to employee EID, along with the project name next to each task
-		  \temp history <EID> - Show completed tasks for employee with id=EID
-		  \tquit or q - Quits the program.\n"
+  			\tusers list - List all users
+  			\tmatch list - List active matches
+  			\tmatch play MID - Start playing game with id=MID
+  			\tinvites - List all pending invites
+  			\tinvite accept IID - Accept invite with id=IID
+  			\tinvite create USERNAME - Invite user with username=USERNAME to play a game
+				\tmatch list"
 		end
 	end
 end
+
+RPS::TerminalClient.start
